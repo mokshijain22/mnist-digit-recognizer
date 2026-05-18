5e0890d8e5154d5e166b287e33268c7cb96ef9a1
mokshijain22 <mokshijain2209@gmail.com>
2026-05-18 21:12:07 +0530

    .

--- app.py
+++ app.py
@@ -267,5 +267,4 @@ def predict_multidigit():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
